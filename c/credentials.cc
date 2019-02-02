1006e8835c27d6feeaa216bf4fad8ac748500e7b
Marco Vedovati <mvedovati@suse.com>
2019-02-02 13:21:02 +0100

    credentials: add default and pio copy

---
+++ c/credentials.cc
@@ -0,0 +1,10 @@
+#include "credentials.hh"
+
+const char *Credentials::wifiSsid = "esp-rulez";
+const char *Credentials::wifiPasswd = "not-so-secret";
+const char *Credentials::mqttBroker = "192.168.1.254";
+const char *Credentials::mqttUsername = "mqtt-user";
+const char *Credentials::mqttPassword = "mqtt-password";
+const char *Credentials::httpUsername = "foo bar";
+const char *Credentials::httpPassword = "cant-crack-me";
+
---
+++ pio/gen-http-credentials.py
@@ -0,0 +1,23 @@
+# Copy a standard credentials file if it does not exist.
+import sys, os, subprocess
+
+Import('env')
+
+CREDENTIALS = {
+    "src" : "./esp-scripts/c/credentials.cc",
+    "dst" : "./src/credentials.cc"
+}
+
+#
+# Dump build environment (for debug)
+#print env.Dump()
+#
+
+if os.path.isfile(CREDENTIALS["dst"]):
+    sys.stdout.write("WARN: credentials file {} already exits.".format(CREDENTIALS["dst"]))
+    sys.exit(0)
+
+retcode = subprocess.call(["cp", CREDENTIALS["src"], CREDENTIALS["dst"]])
+
+if retcode != 0:
+    sys.exit(retcode) 
