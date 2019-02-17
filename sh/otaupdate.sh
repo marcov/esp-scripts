a77491b9f84d0f3e6bd1faa3d466ba61600f8c22
Marco Vedovati <mvedovati@suse.com>
2019-02-17 10:07:11 +0100

    Add no confirmation flag when OTA flashing

--- sh/otaupdate.sh
+++ sh/otaupdate.sh
@@ -3,24 +3,51 @@
 set -euo pipefail
 
 declare -r htmlFormFieldName="update"
-env="d1mini"
+declare boardName="d1mini"
+declare confirm="1"
+declare -r myName="$(basename $0)"
 
-if [ "$#" -lt "1" ] || [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+usage() {
 	cat << EOF
-Usage: $(basename $0) ESP-HOST [ENV]
+Usage:
+$myName [OPTIONS] ESP-HOST [ENV]
+
+OPTIONS  -h show this help and exit
+         -n do not ask for confirmation before flash
 
 ESP-HOST IP/hostname of the ESP
-ENV      name of the board specified as "env=" in platform.ini. Defaults to "${env}"
+
+ENV      name of the board specified as "env=" in platform.ini. Defaults to "${boardName}"
+
 EOF
 	exit -1
+}
+
+if [ "$#" -lt "1" ] || [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+	usage
 fi
 
-espHost="$1"
-updateUrl="${espHost}/update"
+while [ "$#" -ge "1" ]; do
+	param="$1"
 
+	case "$param" in
+		-n)
+			confirm=
+			;;
+		*)
+			espHost="$param"
+			break
+			;;
+	esac
+	shift
+done
+
+[ -z "${espHost:-}" ] && { echo -e "ERROR: no ESP-HOST SPECIFIED\n"; usage; }
+updateUrl="${espHost}/update"
 shift
-[ "$#" -ge "1" ] && env="$1"
-binaryPath=.pioenvs/${env}/firmware.bin
+[ "$#" -ge "1" ] && boardName="$1"
+
+binaryPath=.pioenvs/${boardName}/firmware.bin
 
 if ! [ -f "$binaryPath" ]; then
 	echo "ERR: firmware file $binaryPath does not exist"
@@ -29,16 +56,18 @@ fi
 
 cat <<EOF
 OTA update with the following config:
-	IP   $espHost
-	ENV  $env
+	IP     $espHost
+	BOARD  $boardName
 
 EOF
 
-echo -n "Confirm [y/N]: "
-read readVal
-if [ "$readVal" != "y" ]; then
-	echo "Aborted, exiting"
-	exit -1
+if [ -n "$confirm" ]; then
+	echo -n "Confirm [y/N]: "
+	read readVal
+	if [ "$readVal" != "y" ]; then
+		echo "Aborted, exiting"
+		exit -1
+	fi
 fi
 
 echo ""
